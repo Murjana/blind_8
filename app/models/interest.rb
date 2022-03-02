472549548fa1ba43f2cfaf4d7b17cab9244dd67c
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-03-02 13:01:57 +0100

    updated association and validation

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,2 +1,4 @@
 class Interest < ApplicationRecord
+  has_many :user_interests
+  has_many :user, through: :user_interests
 end
--- app/models/user_interest.rb
+++ app/models/user_interest.rb
@@ -1,4 +1,4 @@
 class UserInterest < ApplicationRecord
   belongs_to :user
-  belongs_to :interests
+  belongs_to :interest
 end
