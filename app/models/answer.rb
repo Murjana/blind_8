8016c9a85c6c7c047fd4ac2a8da494073ad261a8
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-03-02 13:06:50 +0100

    updated validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :asker, class_name: "User"
   belongs_to :replier, class_name: "User"
+
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,12 @@ class User < ApplicationRecord
   has_many :chatrooms_as_user_a, class_name: "Chatroom", foreign_key: :user_a_id
   has_many :chatrooms_as_user_b, class_name: "Chatroom", foreign_key: :user_b_id
   has_many :messages
-  #has_many :messages_as_user_b, through: :chatrooms, source: :messages
   has_many :answers_as_asker, class_name: "Answer", foreign_key: :asker_id
   has_many :answers_as_replier, class_name: "Answer", foreign_key: :replier_id
 
   validates :password, :email, :age, :preferred_date_ideas, presence: true
   validates :name, presence: false
-  validates :about_me, presence: true, :length {in: 30..100 }
+  validates :about_me, presence: true, length: { in: 30..100 }
   validates :nickname, presence: true, uniqueness: true
   validates :orientation, presence: true, inclusion: { in: %w(heterosexual homosexual bisexual) }
   validates :gender, presence: true, inclusion: { in: %w(male female binary) }
