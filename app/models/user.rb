4c1dce54f59ecbd6293f0004a472b76e8e24d5e3
Wes House <wesevan7@gmail.com>
2022-03-04 16:28:45 +0100

    commented line in validation user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_one_attached :photo
 
   validates :password, :email, :age, :preferred_date_ideas, presence: true
-  # validates :about_me, presence: true, length: { in: 30..200}
+      # validates :about_me, presence: true, length: { in: 30..200}
 
   validates :nickname, presence: true, uniqueness: true
   validates :orientation, presence: true, inclusion: { in: %w(heterosexual gay straight pansexual bisexual) }
