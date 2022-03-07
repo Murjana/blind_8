339585435188ce30fb20313ce96e9d8e8aee5c90
Morjana <96773815+Murjana@users.noreply.github.com>
2022-03-07 14:59:19 +0100

    created the index  nethod for chatrooms for current user

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,4 +4,9 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
+
+  def index
+    @chatrooms = current_user.chatrooms.first(8)
+  end
+
 end
