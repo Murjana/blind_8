ee6c4c99304bc526b1c3cc7187f8a42a7c6c1b93
Wes House <wesevan7@gmail.com>
2022-03-07 15:40:06 +0100

    registration forms updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #   sessions: 'users/registrations'
   # }
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
   root to: 'pages#home'
 
